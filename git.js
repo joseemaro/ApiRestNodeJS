f8ce84f9ed0f62493fa3b57dbda0cbd27d8d57ed
joseemaro <joseemaro@hotmail.com>
2021-07-18 14:53:04 -0300

    Primer commit api Rest Express NodeJS

---
+++ git.js
@@ -0,0 +1,10 @@
+/* 
+git --version
+git config --global user.name "joseemaro"
+git config --global user.email "joseemaro@hotmail.com"
+git init
+git add .
+CREAR REPO
+git remote add origin https://github.com/joseemaro/ApiRestNodeJS.git
+git push -u origin master
+*/
